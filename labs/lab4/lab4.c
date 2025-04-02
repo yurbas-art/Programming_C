8d82ebc62ca775d89284d67215e7386b272b7c35
yurbas-art <70734690+yurbas-art@users.noreply.github.com>
2025-04-02 18:34:56 +1000

    Update lab4.c

--- labs/lab4/lab4.c
+++ labs/lab4/lab4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
 
+
+
+int main(){
+    char name_str[81];
+    int i = 0;
+    
+    printf("Введите строку символов произвольной длины(до 80):");
+    scanf("%81[^\n]", name_str);
+    
+    while(name_str[i] != '\0'){
+        name_str[i] = toupper(name_str[i]);
+        i++;
+    }
+    
+    printf("Заглавные буквы: %s\n", name_str);
+    
+    return 0;
+}
