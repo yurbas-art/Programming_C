a8cb414d12aff463f6a10e25ddb7a1dc2d93a43d
yurbas-art <70734690+yurbas-art@users.noreply.github.com>
2025-04-26 17:06:56 +1000

    Update lab7.c

--- labs/lab7/lab7.c
+++ labs/lab7/lab7.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+struct humen{
+    char name[20];
+    char surname[20];
+    int age;
+};
+
+int main(){
+    int i, j, n = 4;
+    struct humen arr1[n], arr2[n];
+    FILE*input = fopen("input7.txt", "r");
+    char buffer[128];
+    for (i = 0; i < n; i++){
+        fgets(buffer, 100, input);
+        sscanf(buffer, "%s %s %d", &arr1[i].name, &arr1[i].surname, &arr1[i].age);
+    }
+    for (i = 0; i < n; i++) arr2[i] = arr1[i];
+
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-1 - i; j++) {
+            if (arr2[j].age > arr2[j + 1].age) {
+                struct humen buffer = arr2[j];
+                arr2[j] = arr2[j + 1];
+                arr2[j + 1] = buffer;
+            }
+        }
+    }
+    printf("Отсортированный список:\n");
+    for (i = 0; i<n; i++) printf("%d. %s %s %d\n", i+1, arr2[i].name, arr2[i].surname, arr2[i].age);
+
+    return 0;
+}
