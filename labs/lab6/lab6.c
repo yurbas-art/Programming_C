0bee36a0093abfd9793d0c443e04e7e8729eda00
yurbas-art <70734690+yurbas-art@users.noreply.github.com>
2025-04-26 17:06:07 +1000

    Update lab6.c

--- labs/lab6/lab6.c
+++ labs/lab6/lab6.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main(){
+    int year1 = 1980;
+    FILE*input;
+    FILE*output;
+    input = fopen("input.txt", "r");
+    output = fopen("output.txt", "w");
+    if (input == NULL || output == NULL) {
+        printf("Ошибка открытия файла.");
+        return 1;
+    }
+    
+    char buffer[100];
+    while((fgets(buffer, 100, input))!=NULL) {
+        int year; char name[50];
+        sscanf(buffer, "%s %*s %*s %*s %d", name, &year);
+        if (year > year1){
+            printf("%s", buffer);
+            fprintf(output, buffer);
+        }
+    } fclose(input); fclose(output);
+    getchar();
+    return 0;
+
+}
