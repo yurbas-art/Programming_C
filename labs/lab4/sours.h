a904ea2255f373f05ec9db64fe369cb94e9b72c1
yurbas-art <70734690+yurbas-art@users.noreply.github.com>
2025-05-17 11:59:10 +1000

    Add files via upload

---
+++ labs/lab4/sours.h
@@ -0,0 +1,11 @@
+#ifndef SOURS_H_INCLUDED
+#define SOURS_H_INCLUDED
+#include <stdbool.h>
+
+double square(double a, double b, double c);
+int perimetr(int a, int b, int c);
+bool check(int a, int b, int c);
+
+
+
+#endif // SOURS_H_INCLUDED
