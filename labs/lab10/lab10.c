f2381c54bb9c65fcf38dd9a4dd722c7e4b47450f
yurbas-art <70734690+yurbas-art@users.noreply.github.com>
2025-05-24 01:00:37 +1000

    Update lab10.c

--- labs/lab10/lab10.c
+++ labs/lab10/lab10.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
 
+
 #define MAX_NUM(arr, n) ({ \
-int max_num = arr[0]; \
-for (int i = 1; i < n; i++) { \
-if (arr[i] > max_num) { \
-max_num = arr[i]; \
-} \
-} \
-max_num; })
-#define AVG(array, n) ({ \
-int sum = 0; \
-for (int i = 0; i < n; i++) { \
-sum += array[i]; \
-} \
-(float)sum / n; \
+    int max = arr[0];      \
+    for (int i = 1; i < n; i++) { \
+        if (arr[i] > max) max = arr[i]; \
+    } \
+    max; \
 })
-int main()
-{
+
+int main() {
     int n;
-    printf("Enter the number of chisel: ");
+    printf("Skolko chisel? ");
     scanf("%d", &n);
 
     int numbers[n];
 
-    printf(" ender number separated by a space: ");
-    for (int i = 0; i < n; i++)
-    {
+    printf("Vvedite chisla cherez probel: ");
+    for (int i = 0; i < n; i++) {
         scanf("%d", &numbers[i]);
     }
 
-    int max_num = MAX_NUM(numbers, n);
-
-    printf("max nubmer: %d\n", max_num );
-    printf("average: %f", AVG(numbers, n));
+    printf("Max number: %d\n", MAX_NUM(numbers, n));
 
     return 0;
 }
