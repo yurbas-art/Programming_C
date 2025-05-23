19b1675cae6302181200cb12f56ccb1b70b3b458
yurbas-art <70734690+yurbas-art@users.noreply.github.com>
2025-05-24 01:36:27 +1000

    Create triangle.h

---
+++ labs/lab4/triangle.h
@@ -0,0 +1,10 @@
+double calculate_perimeter(double a, double b, double c);
+double calculate_area(double a, double b, double c);
+//#ifndef TRIANGLE_H
+//#define TRIANGLE_H
+
+
+
+
+
+//#endif //TRIANGLE_H
