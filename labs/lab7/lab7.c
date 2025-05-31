130643dadfd7412a8a4f7384d3f1a467d6dd3a08
Iurii <70734690+yurbas-art@users.noreply.github.com>
2025-05-31 11:54:13 +1000

    Update lab7.c

--- labs/lab7/lab7.c
+++ labs/lab7/lab7.c
@@ -1,33 +1,67 @@
 #include <stdio.h>
 
-struct humen{
-    char name[20];
-    char surname[20];
-    int birth_year;
+
+#define SIZE 89
+
+// Объявление структуры humen
+struct humen {
+    char name[50];
+    char surname[50];
+    int birthYear;
 };
+int comparefirstletter(char a,char b) {
+    if (a >= 'A' && a <= 'Z') a = a;
+    if (b >= 'A' && b <= 'Z') b = b;
+    return a > b;
+}
 
-int main(){
-    int i, j, n = 8;
-    struct humen arr1[n], arr2[n];
-    FILE*input = fopen("input7.txt", "r");
-    char buffer[128];
-    for (i = 0; i < n; i++){
-        fgets(buffer, 100, input);
-        sscanf(buffer, "%s %s %d", &arr1[i].name, &arr1[i].surname, &arr1[i].birth_year);
-    }
-    for (i = 0; i < n; i++) arr2[i] = arr1[i];
-
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-1 - i; j++) {
-            if (arr2[j].birth_year > arr2[j + 1].birth_year) {
-                struct humen buffer = arr2[j];
-                arr2[j] = arr2[j + 1];
-                arr2[j + 1] = buffer;
+// Функция для сортировки массива по первой букве
+void sortByFirstLetter(struct humen arr[], int size) {
+    struct humen temp;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (comparefirstletter(arr[j].name[0], arr[j+1].name[0])) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
-    printf("Отсортированный список:\n");
-    for (i = 0; i<n; i++) printf("%d. %s %s %d\n", i+1, arr2[i].name, arr2[i].surname, arr2[i].birth_year);
+}
+
+int main() {
+    struct humen people[SIZE];
+    struct humen sortedPeople[SIZE];
+
+    // Открываем файл для чтения
+    FILE *file = fopen("input.txt", "r");
+    if (file == NULL) {
+        printf("can`t open in.txt\n");
+        return 1; // Завершаем программу, если файл не открыт
+    }
+
+    // Чтение данных из файла в массив people
+    for (int i = 0; i < SIZE; i++) {
+        fscanf(file, "%s %s %d", people[i].name, people[i].surname, &people[i].birthYear);
+    }
+
+    // Закрываем файл
+    fclose(file);
+
+    // Копируем данные в массив sortedPeople
+    for (int i = 0; i < SIZE; i++) {
+        sortedPeople[i] = people[i];
+    }
+
+    // Сортируем массив sortedPeople по годам рождения
+    sortByFirstLetter(sortedPeople, SIZE);
+
+    // Вывод результатов
+    printf("\n sorted by first letter in name:\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("name: %s, surname: %s, birthYear: %d\n",
+               sortedPeople[i].name, sortedPeople[i].surname, sortedPeople[i].birthYear);
+    }
 
     return 0;
 }
